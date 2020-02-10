7444695913c6318dbef5c6c1068cbbca8d72fa06
vivianjohnson <59099112+vivianjohnson@users.noreply.github.com>
2020-02-09 20:09:04 -0800

    Lab 3 Complete

---
+++ Vivian Personal Project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // i want to be able to move the ball around in all directions by being able
+    // rotate it using the arrow keys and when it hits the capsuls or cube it jumps
+
+    private float speed = 10.0f;
+    private Rigidbody playerRb;
+ //   private float zBound = 20; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        playerRb.AddForce(Vector3.forward * speed * verticalInput);
+        playerRb.AddForce(Vector3.right * speed * horizontalInput);
+
+       // if(transform.position.z < -zBound)
+       // {
+          //  transform.position = new Vector3(transform.position.x, transform.position.y, -zBound);        
+       // }
+
+      //  if(transform.position.z > zBound)
+    //    {
+          //  transform.position = new Vector3(transform.position.x, transform.position.y, zBound);
+       // }
+    }
+}
